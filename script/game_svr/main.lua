733c162abd498553498bc440772dcf650ded08f6
mashago <mashacoder@foxmail.com>
2017-06-09 07:26:27 +0800

    add connect service

--- script/game_svr/main.lua
+++ script/game_svr/main.lua
@@ -6,10 +6,16 @@ local function main_entry()
 
 	register_msg_handler()
 
+	Services.add_connect_service("127.0.0.1", 7712, "aaa")
+	Services.add_connect_service("127.0.0.1", 7713, "bbb")
+	Services.create_connect_timer(2000)
+
+	--[[
 	local function timer_cb(arg)
 		Log.info("timer_cb arg=%d", arg)
 	end
 	Timer.add_timer(1000, timer_cb, 10086, true)
+	--]]
 end
 
 main_entry()
---
+++ script/sys/services/services.lua
@@ -0,0 +1,53 @@
+
+Services = {}
+
+Services._all_connect_servers = {}
+Services._connect_server_session_id_map = {}
+
+function Services.add_connect_service(ip, port, desc)
+	local server_info = 
+	{
+		_server_id = 0, 
+		_server_type = 0, 
+		_ip = ip, 
+		_port = port, 
+		_desc = desc, 
+		_session_id = -1,
+		_is_registed = false,
+		_is_connecting = false,
+		_is_connected = false,
+		_last_connect_time = 0,
+	}
+	table.insert(Services._all_connect_servers, server_info)
+end
+
+function Services.create_connect_timer(ms)
+
+	local function timer_cb(arg)
+		Log.debug("handle_client_test: Services._all_connect_servers=%s", tableToString(Services._all_connect_servers))
+		
+		--[[
+		local is_all_connected = true
+		for _, server_info in pairs(Services._all_connect_servers) do
+			if not server_info._is_connected then
+				is_all_connected = false
+			end
+			if not server_info._is_connecting then
+				local ret, session_id = g_network:connect_to(server_info._ip, server_info._port)
+				if ret then
+					server_info._session_id = session_id
+					server_info._is_connecting = true
+				end
+
+			end
+		end
+		if is_all_connected then
+			Timer.del_timer(Services._connect_timer_index)
+		end
+		--]]
+	end
+
+	Services._connect_timer_index = Timer.add_timer(ms, timer_cb, 0, true)
+end
+
+return Services
