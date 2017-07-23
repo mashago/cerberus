3cfd3bd3c15e3e6de1a3d43090b419a2e2655b1a
mashago <mashacoder@foxmail.com>
2017-07-24 07:44:47 +0800

    update transfer msg

--- script/router_svr/net_event_handler.lua
+++ script/router_svr/net_event_handler.lua
@@ -17,6 +17,20 @@ end
 function g_net_event_transfer_msg(mailbox_id, msg_id, ext)
 	Log.info("g_net_event_transfer_msg: mailbox_id=%d, msg_id=%d, ext=%d", mailbox_id, msg_id, ext)
 	-- TODO transfer to client or server
+	-- 1. if ext non zero, its from server to client, ext is a role_id. so get user by role_id. check if mailbox is from a server
+	-- 2. if ext is zero, its from client to server. for now, just send to user scene server
+
+	if ext ~= 0 then
+		local role_id = ext
+		local user = g_user_mgr:get_user_by_role_id(role_id)
+		if not user then
+			-- user nil 
+			return
+		end
+		if not user:is_online() then
+			return
+		end
+	end
 end
 
 --[[
