3e1fc3ca15398d88102838273f9eab1d89ef3bbc
mashago <mashacoder@foxmail.com>
2017-08-02 20:43:35 +0800

    update run bat

---
+++ bin/Run.bat
@@ -0,0 +1,18 @@
+@echo off
+
+@echo "Start Login Server..."
+start "login_svr" ServerMt.exe "/conf/server_conf_login.xml"
+
+@echo "Start DB Server..."
+start "db_svr" ServerMt.exe "/conf/server_conf_db.xml"
+
+@echo "Start Bridge Server..."
+start "bridge_svr" ServerMt.exe "/conf/server_conf_bridge.xml"
+
+@echo "Start Router Server..."
+start "router_svr" ServerMt.exe "/conf/server_conf_router.xml"
+
+@echo "Start Scene Server..."
+start "scene_svr" ServerMt.exe "/conf/server_conf_scene.xml"
+
+exit
