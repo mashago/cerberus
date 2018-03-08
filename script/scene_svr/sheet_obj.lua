116b4ac3f43399e7a0f83160b07a7ecd28b379ea
mashago <mashacoder@foxmail.com>
2018-03-09 06:58:59 +0800

    update sheetobj

--- script/scene_svr/sheet_obj.lua
+++ script/scene_svr/sheet_obj.lua
@@ -8,6 +8,7 @@ function SheetObj:ctor(sheet_name, key)
 	assert(self._table_def, "SheetObj:ctor no such sheet " .. sheet_name)
 
 	self._key = key
+	--[[
 	self._key_name = nil
 	self._second_key_name = nil
 	for _, field_def in ipairs(self._table_def) do
@@ -18,6 +19,19 @@ function SheetObj:ctor(sheet_name, key)
 		end
 	end
 	assert(self._key_name, "SheetObj:ctor sheet no key " .. sheet_name)
+	--]]
+
+	
+	self._key_names = {} -- {key_name1, key_name2}
+	local key_num = 0
+	for _, field_def in ipairs(self._table_def) do
+		local key = field_def.key or 0
+		if key ~= 0 then
+			self._key_names[key] = {field_def.field=key}
+			key_num = key_num + 1
+		end
+	end
+	assert(key_num > 0 and key_num == #self._key_names, "SheetObj:ctor sheet key error " .. sheet_name)
 
 end
 
@@ -27,7 +41,7 @@ function SheetObj:get_db_data()
 	{
 		table_name = "role_info",
 		fields = {},
-		self._key_name = self._key
+		conditions = {self._key_names[1] = self._key}
 	}
 
 	local status, ret = g_rpc_mgr:call_by_server_type(ServerType.DB, "db_game_select", rpc_data, self._key)
@@ -56,7 +70,7 @@ function SheetObj:init_data(db_record)
 		end
 	end
 
-	if not self._second_key_name then
+	if #self._key_names == 1 then
 		-- only one row
 		-- init from db_record
 		for k, v in pairs(db_record) do
@@ -73,7 +87,14 @@ function SheetObj:init_data(db_record)
 			for k, v in pairs(row) do
 				sub_attr_map[k] = v
 			end
-			self._attr[sub_attr_map[self._second_key_name]] = sub_attr_map
+
+			local target_pos = self._attr
+			for i=2, #self._key_names do
+				local key = sub_attr_map[self._key_names[i]]
+				target_pos[key] = target_pos[key] or {} 
+				target_pos = target_pos[key] 
+			end
+			-- TODO
 		end
 	end
 end
