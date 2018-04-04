8ffdebc3f38f088c01fa8150edf50094a6e8e7cb
mashago <mashacoder@foxmail.com>
2018-04-04 15:04:31 +0800

    add global msg_handler

---
+++ script/core/global/msg_handler.lua
@@ -0,0 +1,28 @@
+
+g_msg_handler = g_msg_handler or {}
+
+function g_msg_handler.s2s_shake_hand_req(data, mailbox_id)
+	g_funcs.handle_shake_hand_req(data, mailbox_id)
+end
+
+function g_msg_handler.s2s_shake_hand_ret(data, mailbox_id)
+	g_funcs.handle_shake_hand_ret(data, mailbox_id)
+end
+
+function g_msg_handler.s2s_shake_hand_invite(data, mailbox_id)
+	g_funcs.handle_shake_hand_invite(data, mailbox_id)
+end
+
+function g_msg_handler.s2s_rpc_req()
+	Log.err("s2s_rpc_req just take place should not enter")
+end
+
+function g_msg_handler.s2s_rpc_nocb_req()
+	Log.err("s2s_rpc_nocb_req just take place should not enter")
+end
+
+function g_msg_handler.s2s_rpc_ret()
+	Log.err("s2s_rpc_ret just take place should not enter")
+end
+
+return g_msg_handler
