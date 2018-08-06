1278491bab4c8c2f57c37266ef5cc93658cbac87
mashago <mashacoder@foxmail.com>
2018-08-06 09:43:14 +0800

    update coroutine

--- script/core/coroutine/coroutine.lua
+++ script/core/coroutine/coroutine.lua
@@ -8,7 +8,8 @@ end
 function CoroutineMgr:create(func)
 	-- 1. first resume in create, pass the call func, and yield self coroutine
 	-- 2. second resume in logic code, pass params for func, and call func
-	-- 3. if no yield in func, just yield func ret
+	-- 3. if func over, push cor into queue, just yield func ret
+	-- 4. when reuse cor, just resume and pass new call func
 
 	local cor = table.remove(self._cor_list)
 	if cor then
@@ -16,12 +17,12 @@ function CoroutineMgr:create(func)
 		return cor	
 	end
 	cor = coroutine.create(function(f)
-		local params = table.pack(coroutine.yield(coroutine.running()))
+		local params = table.pack(coroutine.yield(cor))
 		while true do
 			local ret = table.pack(f(table.unpack(params)))
-			table.insert(self._cor_list, coroutine.running())
+			table.insert(self._cor_list, cor)
 			f = coroutine.yield(table.unpack(ret))
-			params = table.pack(coroutine.yield(coroutine.running()))
+			params = table.pack(coroutine.yield(cor))
 		end
 	end)
 	return select(2, coroutine.resume(cor, func))
