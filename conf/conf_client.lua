98a1dd90c093d0fd20cfd2f18f41ec4e1764b75f
mashago <mashacoder@foxmail.com>
2018-11-25 09:40:50 +0800

    add client config

---
+++ conf/conf_client.lua
@@ -0,0 +1,21 @@
+return {
+	id = 0,
+	type = 0,
+	ip = "127.0.0.1",
+	port = 0,
+	path = "client",
+
+	connect_to = 
+	{
+	},
+
+	server_list =
+	{
+		{
+			ip = "127.0.0.1",
+			port = 60001,
+			id = 11,
+			type = 1,
+		},
+	},
+}
